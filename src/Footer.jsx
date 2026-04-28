4a84f07eb509de6429630342c3c3b10d8679a9d4
areebayousaf23-commits <areebayousaf23@gmail.com>
2026-04-28 06:52:54 -0700

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+  <footer className="footer">
+    <small>&copy; Copyright by ByteGrad. All rights
+    resrved.</small>
+    <small>Last checked limits: 17 days ago</small>
+  </footer>
+  );
+}
